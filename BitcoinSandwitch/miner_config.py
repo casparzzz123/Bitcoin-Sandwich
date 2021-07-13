63eb81b75238a1d8517396c584e8785608616786
casparzzz123 <80700684+casparzzz123@users.noreply.github.com>
2021-07-13 22:50:00 +0200

    Add files via upload

---
+++ BitcoinSandwitch/miner_config.py
@@ -0,0 +1,13 @@
+"""Configure this file before you start mining. Check wallet.py for
+more details.
+"""
+
+# Write your generated adress here. All coins mined will go to this address
+MINER_ADDRESS = "1qWJVDXpR4A94v958jNEhfP+9MZv67dwQKJJzSu8UDQTnu0Nkc+C/wm+hSlo66AAXdIIWCSkMU9hRz0hf7ydzw=="
+
+# Write your node url or ip. If you are running it localhost use default
+MINER_NODE_URL = "http://localhost:5000"
+
+# Store the url data of every other node in the network
+# so that we can communicate with them
+PEER_NODES = []
